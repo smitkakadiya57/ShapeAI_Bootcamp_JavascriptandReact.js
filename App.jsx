67238c7b496fec97c4e7d0772bd73e8297566a87
smitkakadiya57 <76986207+smitkakadiya57@users.noreply.github.com>
2021-06-05 16:29:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
